2634f937a38db34cda38a7a65e545479288140a6
Sharvanand <thirusharwan@gmail.com>
2017-09-29 23:23:33 -0400

    Router created

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
+const router      = require('./routes/route');
 
 const port        = 3000;
 const app         = express();
+
+// Middlewares...
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api/employees', router);
+
 
 app.get('/', (req, res) =>{
     res.send ("Hello MEAN stack 2  !!!!");
