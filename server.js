dbd33d2f65f3783a9819f3661a093b5f388c1864
Sharvanand <thirusharwan@gmail.com>
2017-09-30 08:24:35 -0400

    Database created

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
 const router      = require('./routes/route');
+const config    = require('./config/database');
 
 const port        = 3000;
 const app         = express();
+
+// Connecting mongoDB ...
+mongoose.connect(config.database, (err) =>{
+if(err){
+console.log("MongoDB not connected.." + err);
+}else{
+console.log("MongoDB is successfully connected.. ");
+}
+});
 
 // Middlewares...
 app.use(bodyParser.json());
