27621551da4a4ae1698e8462ab0999ca040025cb
Sharvanand <thirusharwan@gmail.com>
2017-10-01 23:07:35 -0400

    Models created

--- models/model.js
+++ models/model.js
@@ -12,5 +12,20 @@ const Employee = module.exports = mongoose.model('Employee', empSchema);
 
 module.exports.getEmployees = (callback) =>{
 Employee.find(callback);
-console.log('This is from DB');
+}
+
+module.exports.addEmployee = (newEmp, callback)=>{
+    Employee.create(newEmp, callback);
+}
+
+module.exports.updateEmployee = (empUpdate, id, callback) =>{
+    Employee.findByIdAndUpdate(empUpdate, id, callback);
+}
+
+module.exports.getEmployee = (id , callback) =>{
+    Employee.findById(id, callback);
+}
+
+module.exports.deleteEmployee = (id, callback) =>{
+    Employee.findByIdAndRemove(id, callback);
 }
