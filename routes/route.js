07d0ccfb75a228de2c85963f32f1abc29df34d85
Sharvanand <thirusharwan@gmail.com>
2017-10-01 22:35:00 -0400

    Routes created

--- routes/route.js
+++ routes/route.js
@@ -9,5 +9,50 @@ router.get('/', (req, res) => {
  });
 });
 
+router.post('/',(req, res)=>{
+    var  newEmp =  {
+        name: req.body.name,
+        position : req.body.position,
+        department : req.body.department,
+        salary : req.body.salary
+    };
+    Employee.addEmployee(newEmp,(err,employee)=>{
+        if(err) throw err;
+        res.json(employee);
+
+    });
+});
+
+router.put('/:_id',(req, res) =>{
+    var empUpdate ={
+        name : req.body.name,
+        position : req.body.position,
+        department : req.body.department,
+        salary : req.body.salary
+    }
+    Employee.updateEmployee(req.params._id, empUpdate,(err, employee)=>{
+            if(err) throw err;
+            res.json(employee);
+    });
+
+});
+
+router.get('/:_id', (req, res) =>{
+    Employee.getEmployee(req.params._id, (err, employee) =>{
+        if(err) throw err;
+        res.json(employee);
+    });
+
+});
+
+router.delete('/:_id', (req, res)=>{
+    Employee.deleteEmployee(req.params._id, (err, employee) =>{
+        if(err) throw err;
+        res.json(employee);
+    });
+});
+
+
+
 
 module.exports = router;
